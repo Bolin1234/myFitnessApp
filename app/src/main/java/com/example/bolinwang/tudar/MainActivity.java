9420c3fda8d2ea085de2bb24730cdcbd3f07ea52
Bolin1234 <wangbolin123@hotmail.com>
2018-06-14 13:30:38 -0700

    Initial commit

---
+++ app/src/main/java/com/example/bolinwang/tudar/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.bolinwang.tudar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class  MainActivity extends AppCompatActivity {
+    TextView txt;
+    Button btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        txt = (TextView) findViewById(R.id.mytext);
+        btn = (Button) findViewById(R.id.mybutton);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                txt.setText("");
+            }
+        });
+    }
+
+
+}
+
