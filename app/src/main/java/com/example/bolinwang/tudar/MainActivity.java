baea6b6aed57faea7ae60a1e70aed40f2e36a545
Bolin1234 <wangbolin123@hotmail.com>
2018-06-14 23:37:46 -0700

    login

--- app/src/main/java/com/example/bolinwang/tudar/MainActivity.java
+++ app/src/main/java/com/example/bolinwang/tudar/MainActivity.java
@@ -1,21 +1,24 @@
 package com.example.bolinwang.tudar;
 
-import android.support.v7.app.AppCompatActivity;
+/*import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
+import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class  MainActivity extends AppCompatActivity {
-    TextView txt;
-    Button btn;
+   // TextView txt;
+    //Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        txt = (TextView) findViewById(R.id.mytext);
-        btn = (Button) findViewById(R.id.mybutton);
+        txt = (TextView) findViewById(R.id.nameLogin);
+        txt = (TextView) findViewById(R.id.phoneNoLogin);
+        btn = (Button) findViewById(R.id.signinButton);
 
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -23,9 +26,47 @@ public class  MainActivity extends AppCompatActivity {
                 txt.setText("");
             }
         });
-        //test submission
     }
 
 
+}*/
+
+import android.content.Intent;
+        import android.os.Bundle;
+        import android.support.annotation.NonNull;
+        import android.support.v7.app.AppCompatActivity;
+        import android.text.TextUtils;
+        import android.view.View;
+        import android.widget.Button;
+        import android.widget.EditText;
+
+        import com.google.android.gms.tasks.OnCompleteListener;
+        import com.google.android.gms.tasks.Task;
+        import com.google.firebase.auth.AuthResult;
+        import com.google.firebase.auth.FirebaseAuth;
+
+
+
+public class MainActivity extends AppCompatActivity {
+    private EditText nameLogin;
+    private EditText phoneNoLogin;
+    private Button signinButton;
+    private FirebaseAuth auth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        auth = FirebaseAuth.getInstance();
+
+        nameLogin = (EditText) findViewById(R.id.NameLogin);
+        phoneNoLogin = (EditText) findViewById(R.id.PhoneNoLogin);
+        signinButton = (Button) findViewById(R.id.SigninButton);
+
+        String nameLoginUpload = nameLogin.getText().toString().trim();
+        String phoneNoLoginUpload = phoneNoLogin.getText().toString().trim();
+
+        auth.createUserWithEmailAndPassword(nameLoginUpload, phoneNoLoginUpload);
+    }
 }
 
