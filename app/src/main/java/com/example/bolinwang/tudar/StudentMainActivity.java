104645010c5f26f59d6cb556f35136e8d078926d
Linghao-Lyu <35086722+Linghao-Lyu@users.noreply.github.com>
2018-07-06 15:00:18 -0700

    Submission Jul/06

--- app/src/main/java/com/example/bolinwang/tudar/StudentMainActivity.java
+++ app/src/main/java/com/example/bolinwang/tudar/StudentMainActivity.java
@@ -2,9 +2,11 @@ package com.example.bolinwang.tudar;
 
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.constraint.ConstraintLayout;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class StudentMainActivity extends AppCompatActivity {
@@ -18,16 +20,16 @@ public class StudentMainActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_article_student:
-                    mTextMessage.setText(R.string.title_article_student);
+                    articleStudent();
                     return true;
                 case R.id.navigation_private_training_student:
-                    mTextMessage.setText(R.string.title_private_training_student);
+                    privateTrainingStudent();
                     return true;
                 case R.id.navigation_notifications_student:
-                    mTextMessage.setText(R.string.title_notifications_student);
+                    notificationStudent();
                     return true;
                 case R.id.navigation_ask_question_student:
-                    mTextMessage.setText(R.string.title_ask_question_student);
+                    askQuestionStudent();
                     return true;
             }
             return false;
@@ -42,6 +44,29 @@ public class StudentMainActivity extends AppCompatActivity {
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.NavigationStudent);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+    private void articleStudent(){
+        ConstraintLayout layoutArtiStu = (ConstraintLayout) findViewById(R.id.LayoutArtiStu);
+        //layoutArtiStu.bringToFront();
+    }
+    private void privateTrainingStudent(){
+        //ConstraintLayout layoutPrivStu = (ConstraintLayout) findViewById(R.id.LayoutPrivStu);
+        //layoutPrivStu.bringToFront();
+        //Button btnDiet = (Button)findViewById(R.id.BtnDiet);
+        //Button btnExercise = (Button)findViewById(R.id.BtnExercise);
+        //Button btnChat = (Button)findViewById(R.id.BtnChat);
+        //Button btnVidChat = (Button)findViewById(R.id.BtnVidChat);
+        //Button btnSchedule = (Button)findViewById(R.id.BtnSchedule);
+
+    }
+    private void notificationStudent(){
+        ConstraintLayout layoutNotiStu = (ConstraintLayout) findViewById(R.id.LayoutNotiStu);
+       // layoutNotiStu.bringToFront();
+
+    }
+    private void askQuestionStudent(){
+        ConstraintLayout layoutAskStu = (ConstraintLayout) findViewById(R.id.LayoutAskStu);
+       // layoutAskStu.bringToFront();
     }
 
 }
