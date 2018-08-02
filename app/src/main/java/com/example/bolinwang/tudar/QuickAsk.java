5d1b9cb68ca35e65ab51f7af69f86cb178e62b5c
Linghao-Lyu <35086722+Linghao-Lyu@users.noreply.github.com>
2018-08-01 21:21:58 -0700

    Submission Aug1
    DONE
    -Able to select 3 pics and delete by clicking the pics.
    -
    TODO
    -Upload the url of pics to firebase

--- app/src/main/java/com/example/bolinwang/tudar/QuickAsk.java
+++ app/src/main/java/com/example/bolinwang/tudar/QuickAsk.java
@@ -1,14 +1,24 @@
 package com.example.bolinwang.tudar;
 
+import android.app.ProgressDialog;
+import android.content.ClipData;
 import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -16,26 +26,43 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.UUID;
+import java.util.List;
 
 public class QuickAsk extends AppCompatActivity {
     Button btnBack;
     Button btnSend;
+    Button btnSelectPic;
     EditText editTextMsg;
     FirebaseAuth auth;
     private DatabaseReference mDatabaseUser, mDatabaseQuestion;
-
+    int PICK_IMAGE_MULTIPLE = 1;
+    String imageEncoded;
+    List<String> imagesEncodedList;
     String uid;
+    ImageView imgAsk0;
+    ImageView imgAsk1;
+    ImageView imgAsk2;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quick_ask);
-
+        btnSelectPic = findViewById(R.id.BtnSelectPic);
         btnBack = findViewById(R.id.BtnBack);
         btnSend = findViewById(R.id.BtnSend);
         editTextMsg = findViewById(R.id.editTextMsg);
+        imgAsk0 = (ImageView) findViewById(R.id.ImgAsk0);
+        imgAsk1 = (ImageView) findViewById(R.id.ImgAsk1);
+        imgAsk2 = (ImageView) findViewById(R.id.ImgAsk2);
+
 
         auth = FirebaseAuth.getInstance();
         mDatabaseUser = FirebaseDatabase.getInstance().getReference("User");  //initialize database reference
@@ -87,5 +114,106 @@ public class QuickAsk extends AppCompatActivity {
                 finish();
             }
         });
+        btnSelectPic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                chooseImage();
+            }
+        });
+        imgAsk0.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                imgAsk0.setImageDrawable(null);
+            }
+        });
+        imgAsk1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                imgAsk1.setImageDrawable(null);
+            }
+        });
+        imgAsk2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                imgAsk2.setImageDrawable(null);
+            }
+        });
+    }
+
+
+    private void chooseImage() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE_MULTIPLE);
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        try {
+            if(requestCode == PICK_IMAGE_MULTIPLE && resultCode == RESULT_OK
+                    && data != null ){
+
+                String[] filePathColumn ={MediaStore.Images.Media.DATA};
+                imagesEncodedList = new ArrayList<String>();
+                if(data.getData() != null){
+                    Uri mImageUri=data.getData();
+                    Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(),mImageUri);
+                    imgAsk0.setImageBitmap(bitmap);
+                    // Get the cursor
+                    Cursor cursor = getContentResolver().query(mImageUri,
+                            filePathColumn, null, null, null);
+                    // Move to first row
+                    cursor.moveToFirst();
+
+                    int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+                    imageEncoded  = cursor.getString(columnIndex);
+                    cursor.close();
+                } else {
+                    if (data.getClipData() != null) {
+                        ClipData mClipData = data.getClipData();
+                        ArrayList<Uri> mArrayUri = new ArrayList<Uri>();
+                        for (int i = 0; i < mClipData.getItemCount(); i++) {
+
+                            ClipData.Item item = mClipData.getItemAt(i);
+                            Uri uri = item.getUri();
+                            Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(),uri);
+                            if(i == 0){
+                                imgAsk0.setImageBitmap(bitmap);
+                            } else if(i == 1){
+                                imgAsk1.setImageBitmap(bitmap);
+                            } else
+                                imgAsk2.setImageBitmap(bitmap);
+
+                            mArrayUri.add(uri);
+                            // Get the cursor
+                            Cursor cursor = getContentResolver().query(uri, filePathColumn, null, null, null);
+                            // Move to first row
+                            cursor.moveToFirst();
+
+                            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+                            imageEncoded = cursor.getString(columnIndex);
+                            imagesEncodedList.add(imageEncoded);
+                            cursor.close();
+
+                        }
+
+
+                        Log.v("LOG_TAG", "Selected Images" + mArrayUri.size());
+                        Log.v("LIST1", "address1" + imagesEncodedList.get(1));
+                        Log.v("LIST2", "address2" + imagesEncodedList.get(2));
+                        Log.v("LIST3", "address3" + imagesEncodedList.get(0));
+                    }
+                }
+            } else{
+                Toast.makeText(this, "You haven't picked Image",
+                        Toast.LENGTH_LONG).show();
+            }
+        } catch (Exception e) {
+            Toast.makeText(this, "Something went wrong", Toast.LENGTH_LONG)
+                    .show();
+        }
+
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
